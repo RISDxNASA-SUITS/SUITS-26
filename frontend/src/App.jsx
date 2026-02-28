146acc97f29efcaa91240187cd98ad9a46dacb61
Shivam Hingorani <shivamh@icloud.com>
2026-02-28 13:20:23 -0500

    reorganized files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import PredictiveMaximumRange from './components/PredictiveMaximumRange'
+
+function App() {
+  return (
+    <>
+      <PredictiveMaximumRange />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/PredictiveMaximumRange.jsx
@@ -0,0 +1,9 @@
+export default function PredictiveMaximumRange() {
+  return (
+    <div className="bg-[#293044] flex items-center justify-center py-[26.477px] px-[26.477px] w-full" data-node-id="2631:45995">
+      <p className="flex-1 font-semibold leading-[1.4] text-[26.477px] text-white whitespace-pre-wrap font-inter" data-node-id="2631:45996">
+        Predictive Maximum Range
+      </p>
+    </div>
+  );
+}
